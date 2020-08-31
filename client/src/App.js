6a26c3267f038b626a1e82515779490c2b21aa93
mudit67 <bhardwajmudit006@gmail.com>
2020-08-31 19:00:38 +0530

    configured the app to comunicate between front-end and backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,25 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      searchVal: null
+      searchVal: null,
+      respVal:""
     };
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event){
-    console.log(this.state.searchVal);
+    // console.log(this.state.searchVal);
     this.setState({searchVal: event.target.value});
   };
+  sendRequest(){
+    // debugger
+
+    var resp     
+    fetch('http://localhost:8000/search?q='+this.state.searchVal)
+    .then(response => response.json())
+    .then((data)=> {console.log(data);this.setState({respVal:data.data})} );
+  }
   render(){
+    const { searchVal,respVal } = this.state;
     return (
       <>
         <Nav>
@@ -21,16 +31,15 @@ class App extends React.Component {
               <label htmlFor="search">Search</label> 
               <input type="search" placeholder="Search..." id="search" onChange={this.handleChange} style={{marginLeft: 1 + 'em'}}/>
             </pre>
+             
+              <button type="button" onClick={(e) => {this.sendRequest(e)}}> search </button>
+            {respVal}
           </form>
         </Nav>
-        <h1>
-          This is working again.<br/>
-          After Yarn Build.
-        </h1>
+        
       </>
     );
   }  
-    
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const e = require('express');
 const app = express();
 app.use('/',express.static('./client/build/'));
 
 app.listen(8000, () =>{
     console.log("server is running");
 });
+
+// app.use('localhost:8000/search?q=:ser', ()=>{
+// 	console.log(req.params.ser);
+// 	res.statusCode = 200;
+// 	res.setHeader('Content-Type', 'JSON');
+// 	res.end({Found : true});
+// });
+
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+app.get('/search', function (req, res) {
+	console.log(req.query)
+	//  some  dumm ylogic 
+	
+  res.send(JSON.stringify({ data:req.query.q.toUpperCase()}))
+})
