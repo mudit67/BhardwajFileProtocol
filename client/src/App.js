805d9b74b7d7e66e5a69efebf6948ef4ce3cf379
mudit67 <bhardwajmudit006@gmail.com>
2020-08-31 14:58:03 +0530

    able to store the input and log it

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-
-function App() {
-  return (
-    <h1>
-      This is working again.<br/>
-      After Yarn Build.
-    </h1>
-  );
+import {Nav} from 'reactstrap';
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchVal: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    console.log(this.state.searchVal);
+    this.setState({searchVal: event.target.value});
+  };
+  render(){
+    return (
+      <>
+        <Nav>
+          <form>
+            <pre>
+              <label htmlFor="search">Search</label> 
+              <input type="search" placeholder="Search..." id="search" onChange={this.handleChange} style={{marginLeft: 1 + 'em'}}/>
+            </pre>
+          </form>
+        </Nav>
+        <h1>
+          This is working again.<br/>
+          After Yarn Build.
+        </h1>
+      </>
+    );
+  }  
+    
 }
 
 export default App;
