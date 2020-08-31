6e07a3be88829ba613b4f532611d03d87075af31
mudit67 <bhardwajmudit006@gmail.com>
2020-08-31 13:34:18 +0530

    used yarn build to integrate react with express

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>
+      This is working again.<br/>
+      After Yarn Build.
+    </h1>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const e = require('express');
 const app = express();
-app.use('/',express.static('./public/'));
+app.use('/',express.static('./client/build/'));
 
 app.listen(8000, () =>{
     console.log("server is running");
